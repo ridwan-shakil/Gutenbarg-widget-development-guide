8ceaa7c77a63320d8810397dd2cef21a33723fb0
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2022-09-28 13:02:17 +0600

    Register sidebar widgets

---
+++ Gutenbarg.php
@@ -0,0 +1,41 @@
+
+//===============================================
+// Register sidebar widgets 
+//===============================================
+function halim_theme_slug_widgets_init()
+{
+    // Right sidebar
+    register_sidebar(array(
+        'name'           => __('Right sidebar', 'textdomain'),
+        'id'             => 'right-sidebar',
+        'description'    => __('Widgets in this area will be shown on the right side', 'halim'),
+        'before_widget'  => '<div class="single-sidebar">',
+        'after_widget'   => '</div>',
+        'before_title'   => '<h3>',
+        'after_title'    => '</h3>',
+    ));
+    //   sidebar 1 
+    register_sidebar(array(
+        'name'          => __('Footer 1', 'textdomain'),
+        'id'            => 'footer-1',
+        'description'   => __('Widgets in this area will be shown on footer left side.', 'textdomain'),
+        'before_widget' => '<div class="single-footer footer-logo">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ));
+
+  
+}
+add_action('widgets_init', 'halim_theme_slug_widgets_init');
+
+//===============================================
+// Show widgets in frontend
+//===============================================
+
+  <?php if (is_active_sidebar('footer-2')) : ?>
+          <?php dynamic_sidebar('footer-2'); ?>
+    <?php endif; ?> 
+    
+    
+ 
