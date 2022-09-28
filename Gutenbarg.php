451c9b3a36a436ab9be2a8d61a50749a3c170f95
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2022-09-28 20:38:31 +0600

    New Block and old Widgets

--- Gutenbarg.php
+++ Gutenbarg.php
@@ -1,5 +1,22 @@
 <?php
 //===============================================
+// New Block and old Widgets
+//===============================================
+1) Gutenbarg supports both  " Blocks and Widgets "
+2) Block widgets add more flexiblity 
+3) Older widgets can be used from " Lagecy widgets " block
+4)  Old widget screen can be used by installing "classic editor " plugin or adding below code
+5)  Blocks can be developed by using " React or php "
+
+// Removes Block widget screen and show old widget screen 
+function example_theme_support() {
+    remove_theme_support( 'widgets-block-editor' );
+}
+add_action( 'after_setup_theme', 'example_theme_support' );
+
+
+
+//===============================================
 // Register sidebar widgets 
 //===============================================
 function halim_theme_slug_widgets_init()
