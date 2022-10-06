07a4f1f98ce23ed6f5ce88770eea89d707ccc3e2
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2022-10-06 10:41:19 +0600

    Update Gutenbarg.php

--- Gutenbarg.php
+++ Gutenbarg.php
@@ -57,81 +57,73 @@ add_action('widgets_init', 'halim_theme_slug_widgets_init');
  <?php
 
 // ================================================
-// Pages list Widget ========= 
+//  Widget stractour ========= 
 // ================================================
 
-class page_list extends WP_Widget
-{
-
-    function __construct()
-    {
-
-        parent::__construct(
-            'page_list',  // Base ID
-            'Page List ACF'   // Name
-        );
-
-        add_action('widgets_init', function () {
-            register_widget('page_list');
-        });
-    }
-
-    public $args = array(
-        'before_title'  => '<h4>',
-        'after_title'   => '</h4>',
-        'before_widget' => '<div class="single-sidebar">',
-        'after_widget'  => '</div>'
-    );
-
-    public function widget($args, $instance)
-    {
-
-        echo $args['before_widget'];
-        $title = !empty($instance['title']) ? $instance['title'] : __('Recent posts', 'halim');
-        // $title =  $instance['title']
-?>
-        <h4><?php echo $title; ?></h4>
-        <ul>
-            <?php wp_list_pages('sort_column=menu_order&title_li='); ?>
-        </ul>
-    <?php
-        echo $args['after_widget'];
-    }
-
-    public function form($instance)
-    {
-
-        $instance['title'] = !empty($instance['title']) ? $instance['title'] : __('Pages', 'halim');
-        // $text = !empty($instance['text']) ? $instance['text'] : esc_html__('', 'text_domain');
-    ?>
-        <p>
-            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php echo __('Title:', 'halim'); ?></label>
-            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo  $instance['title']; ?>">
-        </p>
-
-        <!-- <p>
-            <label for="<?php echo esc_attr($this->get_field_id('number')); ?>"><?php echo __('Number of post:', 'halim'); ?></label>
-            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('number')); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="number" value="<?php echo  $instance['number']; ?>">
-        </p> -->
-
-
-<?php
-
-    }
-
-    // public function update($new_instance, $old_instance)
-    // {
-
-    //     $instance = array();
-
-    //     $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
-    //     $instance['text'] = (!empty($new_instance['text'])) ? $new_instance['text'] : '';
-
-    //     return $instance;
-    // }
+// Creating the widget
+class wpb_widget extends WP_Widget {
+ 
+function __construct() {
+parent::__construct(
+ 
+// Base ID of your widget
+'wpb_widget', 
+ 
+// Widget name will appear in UI
+__('WPBeginner Widget', 'wpb_widget_domain'), 
+ 
+// Widget description
+array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), )
+);
 }
-$my_widget = new page_list();
-
+ 
+// Creating widget front-end
+ 
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+ 
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+ 
+// This is where you run the code and display the output
+echo __( 'Hello, World!', 'wpb_widget_domain' );
+echo $args['after_widget'];
+}
+ 
+// Widget Backend
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'wpb_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php
+}
+ 
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+ 
+// Class wpb_widget ends here
+} 
+ 
+// Register and load the widget
+function wpb_load_widget() {
+    register_widget( 'wpb_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
 
 ----------------------------------------------------------------------
 
